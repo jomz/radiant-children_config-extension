86ff5952c3ebce4fb34cdcda67c15965c045f948
Benny Degezelle <hi@monkeypatch.be>
2012-10-22 19:24:54 +0200

    Use alias_method_chain i.o. alias
    Un-breaks compatibility with page_factory and likely other extensions

--- lib/children_config/pages_controller_extensions.rb
+++ lib/children_config/pages_controller_extensions.rb
@@ -1,19 +1,21 @@
 module ChildrenConfig::PagesControllerExtensions
   def self.included(base)
     base.class_eval do
-      alias :original_new :new
-
-      def new
-        unless params[:page_id].blank?
-          parent_page = Page.find(params[:page_id])
-          child_parts = parent_page.part("children_config").try(:content)
-          self.model = model_class.new_with_children_config(config, child_parts)
-        else
-          self.model = model_class.new_with_defaults(config)
-          self.model.slug = '/'
-        end
-        response_for :singular
-      end
+      alias_method_chain :new, :children_config
+      
     end
+  end
+  
+  def new_with_children_config
+    unless params[:page_id].blank?
+      parent_page = Page.find(params[:page_id])
+      yaml_config = parent_page.part("children_config").try(:content)
+      @page = self.model = model_class.new_with_children_config(config, yaml_config)
+      assign_page_attributes
+      response_for :new
+    else
+      new_without_children_config
+    end
+    
   end
 end
